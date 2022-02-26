eb087834ea26c97fed99966333cda54e0472831a
xuchangqing <1214043859@qq.com>
2022-02-26 22:23:54 +0800

    second commit

--- src/main/java/com/pesonal/git.java
+++ src/main/java/com/pesonal/git.java
@@ -3,9 +3,13 @@ package com.pesonal;
 /**
  * @author xuchangqing
  * @date 2022-02-26 22:18
+ *
  */
 public class git {
+
     public static void main(String[] args) {
+
         System.out.println("hello txt");
+        System.out.println("hello txt2");
     }
 }
