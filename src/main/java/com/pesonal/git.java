5b95e1354ae5a81619b38fcb8dd17ba2adbf05c2
xuchangqing <1214043859@qq.com>
2022-02-26 22:21:54 +0800

    first commit

---
+++ src/main/java/com/pesonal/git.java
@@ -0,0 +1,11 @@
+package com.pesonal;
+
+/**
+ * @author xuchangqing
+ * @date 2022-02-26 22:18
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello txt");
+    }
+}
