24a57ea41133071e9d6390344dbc04eb278e2b20
xuchangqing <1214043859@qq.com>
2022-02-26 22:30:25 +0800

    hot-fix commit

--- src/main/java/com/pesonal/git.java
+++ src/main/java/com/pesonal/git.java
@@ -7,5 +7,8 @@ package com.pesonal;
 public class git {
     public static void main(String[] args) {
         System.out.println("hello txt");
+        System.out.println("hello txt");
+        System.out.println("hello txt");
+        System.out.println("hello txt");
     }
 }
